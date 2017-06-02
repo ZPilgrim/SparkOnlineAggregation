705ae155a218412e441ffe300ac5e34e67f5ae50
Qiao Jin <616698865@qq.com>
2017-06-02 13:33:43 +0800

    implement sum and count operator logic, add Spark API inside the aggregation client

--- src/main/java/com/utils/Constants.java
+++ src/main/java/com/utils/Constants.java
@@ -7,6 +7,10 @@ public class Constants {
     public static int SUM_TYPE = 1;
     public static int ERR_TYPE = -1;
 
+    // Operator names.
     public final static String SUM = "sum";
     public final static String COUNT = "count";
+
+    // Local master endpoint.
+    public final static String LOCALHOST = "local";
 }
