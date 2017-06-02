1900d264f645babc2791d2abfd891c8b7f2be9be
Qiao Jin <616698865@qq.com>
2017-06-02 12:09:21 +0800

    Add Aggregation query parsing engine to the codebase

--- src/main/java/com/utils/Constants.java
+++ src/main/java/com/utils/Constants.java
@@ -1,11 +1,12 @@
 package com.utils;
 
 /**
- * Created by zhangweimin on 17/5/15.
+ * @Author Qiao Jin
  */
 public class Constants {
     public static int SUM_TYPE = 1;
-
     public static int ERR_TYPE = -1;
-    public static String SUM_TAG = "sum";
+
+    public final static String SUM = "sum";
+    public final static String COUNT = "count";
 }
